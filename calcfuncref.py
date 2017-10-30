96a418bcdc30293b5203ba309e06029258c7b8e7
David Fraser <davidf@j5int.com>
2017-10-30 10:46:22 +0200

    Restructure to handle needing multiple files
    Added core_resource.src (but still need to parse it)

--- calcfuncref.py
+++ calcfuncref.py
@@ -1,27 +1,52 @@
 #!/usr/bin/env python
 
+import logging
 import os
 import requests
 import re
 
-SRC_NAME = 'openoffice-scfuncs.src'
+SRC_NAME = 'openoffice'
+SRC_FILES = {
+    'openoffice': ['scfuncs.src', 'core_resource.src'],
+}
+
 GIT_HASHES = {
     'openoffice': 'c014b5f2b55cff8d4b0c952d5c16d62ecde09ca1',
 }
+
+RAW_PREFIX = {
+    'openoffice': 'https://github.com/apache/openoffice/raw/%s/' % GIT_HASHES['openoffice'],
+}
+
 SRC_URLS = {
-    'openoffice-scfuncs.src': 'https://github.com/apache/openoffice/raw/%s/main/sc/source/ui/src/scfuncs.src' %
-                              GIT_HASHES['openoffice'],
+    'openoffice': {
+        'scfuncs.src': RAW_PREFIX['openoffice'] + 'main/sc/source/ui/src/scfuncs.src',
+        'core_resource.src': RAW_PREFIX['openoffice'] + 'main/formula/source/core/resource/core_resource.src',
+    },
 }
 
 SCRIPTS_DIR = os.path.dirname(os.path.abspath(__file__))
 CACHE_DIR = os.path.join(SCRIPTS_DIR, '_cache')
 
-def get_src():
-    url = SRC_URLS[SRC_NAME]
-    r = requests.get(url)
-    if r.status_code != 200:
-        raise ValueError("Got unexpected response %s (%s)" % (r.reason, r.status_code))
-    return r.content
+def download_src_files():
+    files = {}
+    logging.info("Obtaining %s files", SRC_NAME)
+    for filename in SRC_FILES[SRC_NAME]:
+        local_filename = os.path.join(CACHE_DIR, '%s-%s' % (SRC_NAME, filename))
+        if os.path.exists(local_filename):
+            logging.info("Reading %s", filename)
+            with open(local_filename, 'rb') as f:
+                files[filename] = f.read()
+        else:
+            logging.info("Downloading %s", filename)
+            url = SRC_URLS[SRC_NAME][filename]
+            r = requests.get(url)
+            if r.status_code != 200:
+                raise ValueError("Got unexpected response %s (%s)" % (r.reason, r.status_code))
+            files[filename] = src = r.content
+            with open(local_filename, 'wb') as f:
+                f.write(src)
+    return files
 
 BLOCK_COMMENT = re.compile('/[*].*[*]/', re.DOTALL)
 RESOURCE_START = re.compile('^\tResource ([A-Z0-9_]*)\n\t{', re.MULTILINE)
@@ -30,7 +55,12 @@ STRING_START = re.compile('^\t\tString ([0-9]*) // (.*)\n\t\t{', re.MULTILINE)
 STRING_END = re.compile('^\t\t};', re.MULTILINE)
 TEXT_STR = re.compile(r'^\s*Text\s*\[\s*([a-zA-Z0-9_-]*)\s*\]\s*=\s*"([^"]*)"\s*;', re.MULTILINE)
 
-def parse_src(src):
+def parse_src_files(src_files):
+    scfuncs_parsed = parse_scfuncs(src_files['scfuncs.src'])
+    return scfuncs_parsed
+
+def parse_scfuncs(src):
+    """Parses OpenOffice scfuncs.src"""
     src = BLOCK_COMMENT.sub('', src)
     src = src.replace('Resource RID_SC_FUNCTION_DESCRIPTIONS1\n{', '')
     src = src.replace('};\n\nResource RID_SC_FUNCTION_DESCRIPTIONS2\n{', '')
@@ -38,8 +68,10 @@ def parse_src(src):
     src = src.rstrip()
     if src.endswith('};'):
         src = src[:src.rfind('};')].rstrip()
+    resources = {}
     for resource_start in RESOURCE_START.finditer(src):
         resource_name = resource_start.group(1)
+        resources[resource_name] = resource = []
         resource_end = RESOURCE_END.search(src, resource_start.start())
         if not resource_end:
             continue
@@ -54,18 +86,14 @@ def parse_src(src):
             for text_str in TEXT_STR.finditer(string_src):
                 text_lang = text_str.group(1)
                 text = text_str.group(2)
-                # print(resource_name, string_name, string_description, text)
+                resource.append((resource_name, string_name, string_description, text_lang, text))
+    return resources
 
 
 if __name__ == '__main__':
+    import logging
+    logging.basicConfig(level=logging.INFO)
     if not os.path.exists(CACHE_DIR):
         os.mkdir(CACHE_DIR)
-    src_file = os.path.join(CACHE_DIR, SRC_NAME)
-    if os.path.exists(src_file):
-        with open(src_file, 'rb') as f:
-            src = f.read()
-    else:
-        src = get_src()
-        with open(os.path.join(CACHE_DIR, SRC_NAME), 'wb') as f:
-            f.write(src)
-    parse_src(src)
+    src_files = download_src_files()
+    print parse_src_files(src_files)
